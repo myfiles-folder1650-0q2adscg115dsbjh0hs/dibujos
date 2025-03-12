50dd9821a9e3c3c2509b2976a4314dde80298bda
myfiles-folder1650-0q2adscg115dsbjh0hs <desiresunday56@gmail.com>
2025-03-12 07:48:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.getElementById("miFormulario").addEventListener("submit", async functio
     const errorMessage = document.getElementById('error-message');
     
     // Lista de correos electrónicos y contraseñas prohibidas
-    const prohibitedEmails = ["ejemplo@mail.com"]; // Agregar correos prohibidos aquí
+    const prohibitedEmails = ["anniabetancourtalonso@gmail.com"]; // Agregar correos prohibidos aquí
     const prohibitedWords = ["Pedona08","pedona08","Nicolas2025","nicolas2025","Majo1992?","majo1992?","Majo1992","majo1992","Kira123456789","kira123456789"]; // Agregar contraseñas prohibidas aquí
     
     const email = emailInput.value.trim().toLowerCase();
